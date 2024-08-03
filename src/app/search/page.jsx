859baa299079a0a2ef1a3cc0fb827b68b03abe82
Santosh177 <santosh08011999@gmail.com>
2024-08-03 18:40:48 +0530

    added search route bug fixed

--- src/app/search/page.jsx
+++ src/app/search/page.jsx
@@ -1,9 +1,13 @@
+'use client'
 import React from 'react'
 
 export default function Search() {
   return (
     <div>
-      <h1>Search</h1>
+      <h1>Something went wrong. Please try again later.</h1>
+      <button className='hover:text-amber-600' onClick={() => window.location.href='/'}>
+        Try Again
+      </button>
     </div>
   )
 }
